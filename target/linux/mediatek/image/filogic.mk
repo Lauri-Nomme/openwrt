dcc4a4f695ae7bf51814333039f757004e1acd6a
Lauri Nomme <lauri.nomme@gmail.com>
2026-09-13 01:04:22 +0300

    mediatek: ship kmod-net-mediatek on bpi-r4-pro devices
    Add the mtk_eth module package to the 8X device packages so the =m driver
    lands in the image (with WED parts inside mtk_eth.ko; mtk_wed_ops stays
    builtin). Enables rmmod/insmod driver swap for RSS rollback.

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -796,7 +796,8 @@ define Device/bananapi_bpi-r4-pro-common
   DEVICE_PACKAGES := kmod-dsa-mxl862xx kmod-hwmon-pwmfan kmod-i2c-mux-pca954x \
 		     kmod-eeprom-at24 kmod-mt7996-firmware kmod-mt7996-233-firmware \
 		     kmod-rtc-pcf8563 kmod-sfp kmod-usb3 e2fsprogs f2fsck mkf2fs \
-		     mt7988-wo-firmware kmod-gpio-pca953x kmod-nvme
+		     mt7988-wo-firmware kmod-gpio-pca953x kmod-nvme \
+		     kmod-net-mediatek
   IMAGES := sysupgrade.itb
   KERNEL_LOADADDR := 0x46000000
   KERNEL_INITRAMFS_SUFFIX := -recovery.itb
